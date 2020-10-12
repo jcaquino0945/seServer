51e68097706fcba14da483fc312fc08b84db89cd
Miguel Aquino <jcaquino0945@gmail.com>
2020-10-12 17:42:52 +0800

    Authentication w Passport

---
+++ authenticate.js
@@ -0,0 +1,7 @@
+passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user');
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
